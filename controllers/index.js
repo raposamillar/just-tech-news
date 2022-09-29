7b8d7c30b24e314df4c66feb62ee074c07698912
raposamillar <lisa.raposamillar@gmail.com>
2022-09-29 13:45:42 -0400

    add logic to create and destroy back-end session

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
